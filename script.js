6be37d4cc7cf8ab86068dab70be60cda923883ee
LucasFRodriguez <lucasfrz26@gmail.com>
2025-08-01 15:29:15 -0300

    finalizado

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
 
 const key = "e9edf2d50b58dadea7ba8fe72fd0a0b7";
 
-function cliqueiNoBotao(){
-    const input = document.querySelector(".input-cidade").value
+function colocaDadosNaTela(dados){
 
-    console.log(input)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}       
+
+async function buscarCidade(cidade){
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    colocaDadosNaTela(dados)
+}
+
+function cliqueiNoBotao(){
+    const cidade = document.querySelector(".input-cidade").value
+    buscarCidade(cidade)
 }
